ec09d66507c80e40b4b4a5096570979e74ce2d60
Dumindu <dumindukm@gmail.com>
2023-12-06 22:03:54 +1300

    create CourierOrder with parcels

---
+++ CourierLib.Tests/CourierOrderUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using CourierLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierLib.Tests
+{
+    public class CourierOrderUnitTest
+    {
+        [Fact]
+        public void OrderHasAtleastOneParcel()
+        {
+            decimal dimension = 10;
+
+            List<Parcel> parcel = new List<Parcel>();
+            parcel.Add(Parcel.Create(ParcelType.Create(dimension), dimension));
+
+            CourierOrder courierOrder = CourierOrder.Create(parcel);
+
+            Assert.NotEmpty(courierOrder.Parcels);
+        }
+    }
+}
---
+++ CourierLib/Core/CourierOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierLib.Core
+{
+    public class CourierOrder
+    {
+        private List<Parcel> _Parcels = new List<Parcel>();
+        public CourierOrder(List<Parcel> parcels) 
+        {
+            _Parcels = parcels;
+        }
+
+        public static CourierOrder Create(List<Parcel> parcel)
+        {
+            var courierOrder = new CourierOrder(parcel);
+            return courierOrder;
+        }
+
+        public IList<Parcel> Parcels { get { return _Parcels.AsReadOnly(); } }
+    }
+}
